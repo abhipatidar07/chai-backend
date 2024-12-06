36df0423264db89ad6d8c3a98de7af0e75fdb504
abhipatidar07 <patidarabhishek831@gmail.com>
2024-12-07 01:59:43 +0530

    controller

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import cookieParser from "cookies-parser"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -13,5 +13,13 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes
+
+import  UserRouter from './routes/user.routes.js'
+
+
+//routes decleration
+app.use("/api/v1/users",UserRouter)
 
 export { app }
